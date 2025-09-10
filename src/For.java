fc1e63b25dd5b4d90363a33ef18e8ee68ad18996
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-09-10 11:54:16 +0900

    for practice

--- src/For.java
+++ src/For.java
@@ -1,7 +1,9 @@
 public class For {
     public static void main(String[] args) {
-        for (int i =1; i <= 5; i++) {
-            System.out.println(i);
+        for (int i = 1; i <= 50; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
         }
     }
 }
--- src/For2.java
+++ src/For2.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class For2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("정수를 입력하세요: ");
+        int number =  scanner.nextInt();
+
+        int sum = 0;
+
+        for (int i  = 1; i <= number; i++) {
+            sum += i;
+        }
+
+        System.out.println("1부터" + number +"까지의 합: " + sum);
     }
 }
