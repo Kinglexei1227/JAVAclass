1f63b21b4f238091e6b87cc66e6835d909577906
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-09-16 18:45:56 +0900

    java

---
+++ src/OJ.java
@@ -0,0 +1,15 @@
+class SuperA extends Object {
+}
+
+class MyA extends SuperA {
+    public MyA() {  //파라미터를 받지 않는 디펄트 생성자 생략 가능
+        super(); //생략이 가능
+    }
+}
+
+
+public class OJ {
+    public static void main(String[] args) {
+
+    }
+}
