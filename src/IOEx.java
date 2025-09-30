557c187f99fca36401bd9d1d49f2cf541ff5782b
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-09-30 10:06:15 +0900

    java

---
+++ src/IOEx.java
@@ -0,0 +1,33 @@
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class IOEx {
+
+    public static void main(String[] args) throws Exception {
+        //(1) byte stream 사용
+        // 파일에 데이터 쓰기
+
+        FileOutputStream fos
+                = new FileOutputStream("C://javatest/test.txt");
+        BufferedOutputStream bos
+                = new BufferedOutputStream(fos);
+        String str = "ABC가나다";
+        byte[] r = str.getBytes(); //문자열 -> 바이트배열 변환
+        bos.write(r);
+        bos.flush(); //버퍼에 쌓인거 밀어내기
+        //문자열 -> 바이트배열 변환
+//      fos.write('가'); //파일에 쓰기 제대로 동작하지 않음 => 한글표현을 위해 최
+
+        //파일에서 데이터 읽기
+        FileInputStream fis
+                = new FileInputStream("C://javatest/test.txt");
+        byte[] temp = new byte[1024];
+        fis.read(temp); // 1024 바이트 읽기 -> temp에 저장
+        //바이트배열 -> 문자열 변환
+        String msg = new String(temp);
+        System.out.print(msg.trim() ); //빈공백 제거 후 출력
+        System.out.println("end");
+
+    }
+}
