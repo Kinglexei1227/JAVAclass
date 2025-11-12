534aadb11d125d034a494abcd5a87857c7b946ad
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-11-12 15:21:01 +0900

    java

--- src/Chatting/Client.java
+++
@@ -1,4 +0,0 @@
-package Chatting;
-
-public class Client {
-}
--- src/Chatting/Server.java
+++
@@ -1,9 +0,0 @@
-package Chatting;
-
-public class Server {
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.start();
-    }
-    public void start() {}
-}
