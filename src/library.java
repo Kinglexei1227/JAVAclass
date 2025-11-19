d88d813f251954a13ac903417129b57fe354e29e
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-11-19 15:06:34 +0900

    java

---
+++ src/library.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+class student {
+    public void studentBorrowBooks() {
+    }
+}
+class library {
+    book[] books;
+}
+class book {
+}
+
+
+public class library {
+    public static void main(String[] args) {
+        student s = new student();
+        s.studentBorrowBooks();
+    }
+}
