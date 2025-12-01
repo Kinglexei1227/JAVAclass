1a402154983c87bb76c8d3fb0e9c85e74b7d1572
Kinglexei1227 <whoiskevin21@g.yju.ac.kr>
2025-12-01 15:43:13 +0900

    java

---
+++ src/Animal.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Animal {
+    public static void main(String[] args) {
+
+        ArrayList squares = new ArrayList();
+        squares.add(new square());
+        squares.add(new )
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 100;
+        Interger i = new Interger(10);
+        i = 100;
+
+        double d = 0.1;
+        Double dd = new Double(0.1);
+
+        float f = 0.1f;
+        Float ff = new Float(0.1f);
+
+
+    }
+}
